0d936da49948185ac9884d1f3dbc8e5ec21b4a9c
SahanDhanujaya <sahandhanujaya18@gmail.com>
2026-08-29 00:38:53 +0530

    modified: the reivews

--- src/components/reviews/FeaturableReview.tsx
+++ src/components/reviews/FeaturableReview.tsx
@@ -1,19 +1,18 @@
 'use client';
+import Script from 'next/script';
 import "react-google-reviews/dist/index.css";
 
 function Reviews() {
-
   return (
     <div>
       <div
         id="featurable-1f34bc2c-ea93-4d15-8c91-3510578a5ee6"
         data-featurable-async
       ></div>
-      <script
+      <Script
         src="https://cdn.featurable.com/widget/v2/embed.js"
-        defer
-        charSet="UTF-8"
-      ></script>
+        strategy="afterInteractive"
+      />
     </div>
   );
 }
